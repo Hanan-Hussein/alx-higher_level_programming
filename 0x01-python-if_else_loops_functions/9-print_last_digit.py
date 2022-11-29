a1be6297cd70cb48cab508215bb3baf22573f58d
Hanan-Hussein <hanhuss035@gmail.com>
2022-11-29 09:14:30 -0800

    Addition

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,11 +4,11 @@ def print_last_digit(number):
         new_number = abs(number)
         answer = new_number % 10
         print(answer, end="")
-        print()
+        print("")
         return answer
     else:
         answer = number % 10
         print(answer, end="")
-        print()
+        print("")
         return answer
 
