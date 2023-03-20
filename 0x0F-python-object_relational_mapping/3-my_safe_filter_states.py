3d75f8a6a5b22c6a627150998d3e3d67a2c03410
Hanan-Hussein <hanhuss035@gmail.com>
2023-03-20 21:13:37 +0000

    ft:state-create model state fetch

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# script that lists all 'states' from the database 'hbtn_0e_0_usa'
+# script that takes in arguments and displays,
+# all values in the 'states' table of 'hbtn_0e_0_usa',
+# where 'name' matches the argument
 """
    import 'sys' & 'MySQLdb'
 """
@@ -10,4 +12,4 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
