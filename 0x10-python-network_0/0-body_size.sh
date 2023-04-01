eaa3b20846093fb94c4c1d258f83b25a4548e80c
Hanan-Hussein <hanhuss035@gmail.com>
2023-04-01 20:26:20 +0000

    document

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#takes in URL, sends a request to url and display size of response
 curl -sI "$1" | grep Content-Length | cut -d " " -f 2
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#takes in url and sends GET request to the URL & display body of response
 curl -sL "$1"
